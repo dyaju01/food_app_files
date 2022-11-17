204636b0e32c51990c08b93eb2c1cdafa6b0062c
Tanay <tanaypant71@gmail.com>
2022-11-17 22:39:52 +0530

    Initial commit

---
+++ lib/authentication/auth_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:food_app_files/authentication/Register.dart';
+import 'package:food_app_files/authentication/login.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Colors.yellowAccent,
+                Colors.orange,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            )),
+          ),
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "Dyaju",
+            style: TextStyle(
+              fontSize: 38,
+              color: Colors.white,
+              fontFamily: "Lobster",
+            ),
+          ),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+                text: "Login",
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                text: "Register",
+              ),
+            ],
+            indicatorColor: Colors.black45,
+            indicatorWeight: 6,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.yellowAccent,
+                Colors.orange,
+              ],
+            ),
+          ),
+          child: const TabBarView(
+            children: [
+              LoginScreen(),
+              RegisterScreen(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splashScreen/splash_Screen.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:food_app_files/authentication/auth_screen.dart';
+import 'package:food_app_files/global/global.dart';
+import 'package:food_app_files/mainScreens/home_screen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 8), () async {
+      //if seller is already loggedIn
+      if (firebaseAuth.currentUser != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      }
+      //if seller is not loggedIn
+      else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const AuthScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.asset("images/splash.jpg"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Sell Food Online",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 40,
+                  fontFamily: "Signatra",
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
