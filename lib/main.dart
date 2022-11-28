2282d5bf59af4a08fd598bcd32b5c432c99a682c
Tanay <tanaypant71@gmail.com>
2022-11-28 23:19:39 +0530

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sellers App ',
+      title: 'Sellers Dyaju ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
